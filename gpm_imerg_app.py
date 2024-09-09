3ce58696d976a3376856b9301500e689f48639f6
VMantas <vasco.mantas@gmail.com>
2024-09-09 14:33:12 +0100

    Update gpm_imerg_app.py

--- gpm_imerg_app.py
+++ gpm_imerg_app.py
@@ -8,7 +8,7 @@ import tempfile
 st.title("GPM IMERG Final Precipitation Data")
 
 # Upload NetCDF file
-uploaded_file = st.file_uploader("Upload GPM IMERG Final NetCDF File", type="nc4")
+uploaded_file = st.file_uploader("Upload GPM IMERG Final NetCDF File", type="nc")
 
 if uploaded_file:
     # Save the uploaded file to a temporary file on disk
@@ -36,30 +36,30 @@ if uploaded_file:
         st.write("Time Dimension Info:", time_dim)
         st.write("Time Dimension Shape:", time_dim.shape)
 
-    # Make sure time_dim is valid
-    if time_dim.size > 0:
-        # Select a time slice for visualization
-        time_index = st.slider("Select Time Index", 0, len(time_dim) - 1, 0)
+        # Make sure time_dim is valid
+        if time_dim.size > 0:
+            # Select a time slice for visualization
+            time_index = st.slider("Select Time Index", 0, len(time_dim) - 1, 0)
 
-        # Select a geographic region to display
-        lat = nc.variables['lat'][:]
-        lon = nc.variables['lon'][:]
-        lat_range = st.slider("Latitude Range", float(lat.min()), float(lat.max()), (lat.min(), lat.max()))
-        lon_range = st.slider("Longitude Range", float(lon.min()), float(lon.max()), (lon.min(), lon.max()))
+            # Select a geographic region to display
+            lat = nc.variables['lat'][:]
+            lon = nc.variables['lon'][:]
+            lat_range = st.slider("Latitude Range", float(lat.min()), float(lat.max()), (lat.min(), lat.max()))
+            lon_range = st.slider("Longitude Range", float(lon.min()), float(lon.max()), (lon.min(), lon.max()))
 
-        # Filter data by selected region
-        lat_filter = np.where((lat >= lat_range[0]) & (lat <= lat_range[1]))[0]
-        lon_filter = np.where((lon >= lon_range[0]) & (lon <= lon_range[1]))[0]
-        selected_precip_data = precip_data[time_index, lat_filter, :][:, lon_filter]
+            # Filter data by selected region
+            lat_filter = np.where((lat >= lat_range[0]) & (lat <= lat_range[1]))[0]
+            lon_filter = np.where((lon >= lon_range[0]) & (lon <= lon_range[1]))[0]
+            selected_precip_data = precip_data[time_index, lat_filter, :][:, lon_filter]
 
-        # Plot the data
-        st.subheader("Precipitation Data Visualization")
-        fig, ax = plt.subplots()
-        im = ax.imshow(selected_precip_data, cmap='Blues', aspect='auto', origin='lower')
-        plt.colorbar(im, ax=ax, label="mm/h")
-        st.pyplot(fig)
-    else:
-        st.error("The time dimension is empty or invalid.")
+            # Plot the data
+            st.subheader("Precipitation Data Visualization")
+            fig, ax = plt.subplots()
+            im = ax.imshow(selected_precip_data, cmap='Blues', aspect='auto', origin='lower')
+            plt.colorbar(im, ax=ax, label="mm/h")
+            st.pyplot(fig)
+        else:
+            st.error("The time dimension is empty or invalid.")
 
     # Close the NetCDF file
     nc.close()
